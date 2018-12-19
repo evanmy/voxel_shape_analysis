7e997e377ff418a1773cf500bd36568158f904e5
Evan Ma Yu <emy24@en-ec-sablab-gpu02.coecis.cornell.edu>
2018-12-19 14:49:44 -0500

    fixed loss

--- functions/training_tools.py
+++ functions/training_tools.py
@@ -2,15 +2,23 @@ import torch
 import numpy as np
 import torch.nn.functional as F
 
-def dice_loss(input, target):
-    smooth = 1.
-
-    iflat = input.view(-1)
-    tflat = target.view(-1)
-    intersection = (iflat * tflat).sum()
+def dice_loss(pred, target):
+    eps = 1.
     
-    return 1 - ((2. * intersection + smooth) /
-              (iflat.sum() + tflat.sum() + smooth))
+    if len(target.size())==4:
+        n,c,x,y = target.size()
+    if len(target.size())==5:
+        n,c,x,y,z = target.size()
+
+    target = target.view(n,c,-1)
+    pred = pred.view(n,c,-1)
+    num = torch.sum(2*(target*pred),2) + eps
+    den = (pred*pred).sum(2) + (target*target).sum(2) + eps
+    dice_loss = 1-num/den
+    total_avg = torch.mean(dice_loss)
+    
+    return total_avg
+
 
 def global_loss(w, z1, z2, out1, out2, template, lmdb):
     z_loss = torch.mean(w*torch.mean(torch.abs(z1-z2),1))
